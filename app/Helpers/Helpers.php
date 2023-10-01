864d3adc5f5bad45c1130dda01df2c7c7fd5f24a
smallzee <tech4all583@gmail.com>
2023-10-01 11:52:47 +0100

    update

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,5 @@
+<?php
+
+function static_asset($src){
+    return asset('assets/images/'.$src);
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('account_number');
+            $table->double('balance')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
