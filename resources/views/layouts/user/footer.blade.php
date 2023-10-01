1aa292d340b115328ea4294378dee791e7d17ec7
smallzee <tech4all583@gmail.com>
2023-10-01 13:39:51 +0100

    update

---
+++ resources/views/layouts/user/footer.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $page_title }}</title>
+
+    <!-- Fonts css load -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link id="fontsLink" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Layout config Js -->
+    <script src="{{asset('assets/user/js/layout.js')}}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{asset('assets/user/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+    <!-- Icons Css -->
+    <link href="{{asset('assets/user/css/icons.min.css')}}" rel="stylesheet" type="text/css">
+    <!-- App Css-->
+    <link href="{{asset('assets/user/css/app.min.css')}}" rel="stylesheet" type="text/css">
+    <!-- custom Css-->
+    <link href="{{asset('assets/user/css/custom.min.css')}}" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+
+
+
+<!-- JAVASCRIPT -->
+<script src="{{asset('assets/user/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/user/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/user/js/plugins.js')}}"></script>
+
+<!-- list js-->
+<script src="{{asset('assets/user/libs/list.js/list.min.js')}}"></script>
+<script src="{{asset('assets/user/libs/list.pagination.js/list.pagination.min.js')}}"></script>
+
+<!-- apexcharts -->
+<script src="{{asset('assets/user/libs/apexcharts/apexcharts.min.js')}}"></script>
+
+<!--dashboard crm init js-->
+<script src="{{asset('assets/user/js/pages/dashboard-crm.init.js')}}"></script>
+
+<!-- App js -->
+<script src="{{asset('assets/user/js/app.js')}}"></script>
+</body>
+</html>
